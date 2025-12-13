b43d7e4fea5c2d656a7340451968b4a212eed511
noouh1 <ae2303@fayoum.edu.eg>
2025-12-13 22:05:31 +0200

    Revert

--- Web.Api/obj/Debug/net8.0/Web.Api.AssemblyInfo.cs
+++ Web.Api/obj/Debug/net8.0/Web.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb0bfc28c5d9124ea9b8b950ccf88c57277664b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+607753edb491dc400187fa7068d9eeed9f65ad09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
