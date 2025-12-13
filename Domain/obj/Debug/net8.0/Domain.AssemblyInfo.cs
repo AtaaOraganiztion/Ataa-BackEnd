a2241f03aa35284da19b42b830d9b5f024228a7e
noouh1 <ae2303@fayoum.edu.eg>
2025-12-13 19:47:52 +0200

    fix

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df149eca819516ec627ab9df71dc7c2e9d17a2ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+607753edb491dc400187fa7068d9eeed9f65ad09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
